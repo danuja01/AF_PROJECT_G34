62e98ca4db6667de2c7a9d700c49c1f21b3e4a83
Omesh Sachethana <sachethana12@gmail.com>
2023-05-19 13:01:14 +0530

    checked action types

--- frontend/src/actions/posts.jsx
+++ frontend/src/actions/posts.jsx
@@ -8,7 +8,7 @@ export const getPost = (id) => async (dispatch) => {
     try {
       //dispatch({ type: START_LOADING });
   
-      const { data } = await api.fetchPost(id);
+      const { data } = await api.fetchPost(id); //Fetch a post from the server
   
       dispatch({ type: FETCH_POST, payload: data });
     } catch (error) {
@@ -24,9 +24,9 @@ export const getPosts = () => async (dispatch) => {
 
         //dispatch({ type: START_LOADING });
 
-        const { data } = await api.fetchPosts(); //data represents the posts
+        const { data } = await api.fetchPosts(); //Fetch posts from the server
     
-        dispatch({ type: FETCH_ALL, payload: data });
+        dispatch({ type: FETCH_ALL, payload: data }); // Dispatch an action with the fetched posts
         //dispatch({ type: END_LOADING });
     } catch (error) {
         console.log(error.message);    
@@ -39,9 +39,9 @@ export const getPosts = () => async (dispatch) => {
 export const getPostsBySearch = (searchQuery) => async (dispatch) => {
     try {
         //dispatch({ type: START_LOADING });
-        const data = await api.fetchPostsBySearch(searchQuery);
+        const data = await api.fetchPostsBySearch(searchQuery); // Fetch posts based on search query
 
-        dispatch({ type: FETCH_BY_SEARCH, payload: data.data });
+        dispatch({ type: FETCH_BY_SEARCH, payload: data.data }); // Dispatch an action with the searched posts
         //dispatch({ type: END_LOADING });
         console.log(data)
     } catch (error) {
@@ -54,9 +54,9 @@ export const getPostsBySearch = (searchQuery) => async (dispatch) => {
 export const createPost = (post) => async (dispatch) => {
     try {
         //dispatch({ type: START_LOADING });
-        const data  = await api.createPost(post);
+        const data  = await api.createPost(post); // Send the new post data to the server and create a new post
         
-        dispatch({ type: CREATE, payload: data })
+        dispatch({ type: CREATE, payload: data }) // Dispatch an action with the newly created post
     } catch (error) {
         console.log(error);
     }
@@ -65,9 +65,9 @@ export const createPost = (post) => async (dispatch) => {
 //update action
 export const updatePost = (id, post) => async (dispatch) => {
     try {
-        const { data} = await api.updatePost(id, post);
+        const { data} = await api.updatePost(id, post); // Send the updated post data to the server and update the post
         
-        dispatch( { type: UPDATE, payload: data })
+        dispatch( { type: UPDATE, payload: data }); // Dispatch an action with the updated post
     } catch (error) {
         console.log(error);
     }
@@ -76,9 +76,9 @@ export const updatePost = (id, post) => async (dispatch) => {
 //delete action
 export const deletePost = (id) => async (dispatch) => {
     try {
-        await api.deletePost(id);
+        await api.deletePost(id); // Send a request to the server to delete the post
 
-        dispatch({ type: DELETE, payload: id });
+        dispatch({ type: DELETE, payload: id }); // Dispatch an action to delete the post from the state
     } catch (error) {
         console.log(error);
     }
@@ -87,9 +87,9 @@ export const deletePost = (id) => async (dispatch) => {
 //like action
 export const likePost = (id) => async (dispatch) => {
     try {
-      const { data } = await api.likePost(id);
+      const { data } = await api.likePost(id); // Send a request to the server to like the post
   
-      dispatch({ type: LIKE, payload: data });
+      dispatch({ type: LIKE, payload: data }); // Dispatch an action with the updated post (with the new like)
     } catch (error) {
       console.log(error.message);
     }
